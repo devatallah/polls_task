7c1a3b9dd4d5758620e165126db5c1fa6d293b9b
devata <dev.atallah@gmail.com>
2023-01-03 17:48:30 +0200

    bind observer appserviceprovide

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Choice;
+use App\Observers\ChoiceObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Choice::observe(ChoiceObserver::class);
     }
 }
